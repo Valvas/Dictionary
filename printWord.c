8a4bcac237efdaad169c502052b02dedf7e20f3c
Valvas <vincent.valvas@outlook.fr>
2016-12-06 09:04:25 +0100

    PRINT_WORDS_IN_TEST_WITH_FILES_ADDED

---
+++ printWord.c
@@ -0,0 +1,20 @@
+#include "functions.h"
+
+/**
+
+
+
+**/
+
+void printWord(word* wrd)
+{
+	if(wrd)
+	{
+		printf("\n======================================================================\n");
+		printf("\n%s\n",wrd->title);
+		printf("\n          %s\n",wrd->definition);
+		printf("\n                    %s\n",wrd->origin);
+		
+		printWord(wrd->nextWord);
+	}
+}
